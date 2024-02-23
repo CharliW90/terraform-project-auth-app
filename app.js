cc419a197327d88b51ccdd0c4442f682410be1b5
Carrie Platten <cjplatten@gmail.com>
2024-02-23 14:52:12 +0000

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require("cors");
 const credentials = require("./credentials")
 
 const app = express()
 app.use(express.json());
+app.use(cors());
 
 app.post('/api/auth/login', (req, res) => {
     const { body } = req
